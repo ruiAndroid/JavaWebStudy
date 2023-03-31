e9bcc9ad3a6212caba63bfdfbbae90e894428520
jianrui <jianrui@fun.tv>
2023-03-31 15:04:23 +0800

    ioc 第一次提交

---
+++ IOC01/src/main/java/model/Main.java
@@ -0,0 +1,9 @@
+package model;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    }
+}
---
+++ IOC01/src/main/java/model/User.java
@@ -0,0 +1,33 @@
+package model;
+
+public class User {
+
+    private String userName;
+    private String address;
+    private String id;
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
