55e8c694389e898f1791005fecd82f4fb3321dc8
jianrui <jianrui@fun.tv>
2023-08-17 19:34:28 +0800

    全局异常拦截

---
+++ IOC01/src/main/java/springmvc/exception/ExceptionHandler.java
@@ -0,0 +1,20 @@
+package springmvc.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 异常处理
+ */
+
+@ControllerAdvice
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+    public ModelAndView fileUploadSizeLimitException(Exception exception){
+        ModelAndView modelAndView=new ModelAndView("/error");
+        modelAndView.addObject("error",exception.getMessage());
+        return modelAndView;
+
+
+    }
+}
